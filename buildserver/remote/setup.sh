f1506215b7934a8603e33515b1115b3c6a119a18
MDJ-Mikkel-Djurhuus <mikkel.djurhuus@gmail.com>
2017-11-16 14:56:24 +0100

    setup

--- buildserver/remote/setup.sh
+++ buildserver/remote/setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
 
-# Important, run this script as sudo
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 sudo apt-get update
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
+sudo apt-get install docker-ce
 sudo usermod -aG docker $(whoami)
-sudo apt-get install -y docker-compose
 
 # Installing the deployment script, which we will call from a Jenkins build job
 

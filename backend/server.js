480ae4824f2265160e2dd2dbf36ab60c6fcedfe7
MDJ-Mikkel-Djurhuus <mikkel.djurhuus@gmail.com>
2017-11-21 08:46:51 +0100

    prometheus

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const index = require("./routes/index.js");
 const post = require("./routes/post.js");
 const user = require("./routes/user.js");
+const metrics = require("./routes/metrics.js");
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', index);
 app.use('/post', post);
 app.use('/user', user);
+app.use('/metrics', metrics);
 
 app.listen(8081, function() {
     console.log("listening on 8081")

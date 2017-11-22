8b5f9719edeb8bb9629c0d0463cefc5f58e11e7d
MDJ-Mikkel-Djurhuus <mikkel.djurhuus@gmail.com>
2017-11-22 18:19:32 +0100

    requirements

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,23 +5,27 @@ var Post = require("../models/post");
 var User = require("../models/user");
 
 
-router.get('/latest', function(req, res, next) {
-    Post.getLatest(function(err, rows) {
+router.get('/latest', function (req, res, next) {
+    Post.getLatest(function (err, rows) {
         if (err) {
             res.json(err);
         } else {
-            res.json(rows[0].hanesst_id);
+            if (rows.length > 0)
+                res.json(rows[0].hanesst_id);
+            else
+                res.json("No posts created yet");
         }
         next()
 
     });
 });
-router.get('/status', function(req, res, next) {
-    res.json("UP");
+router.get('/status', function (req, res, next) {
+    res.json("Alive");
+    next()
 });
 
-router.post('/login', function(req, res, next) {
-    User.login(req.body, function(err, rows) {
+router.post('/login', function (req, res, next) {
+    User.login(req.body, function (err, rows) {
         if (err) {
             res.json(err);
         } else {
@@ -32,9 +36,9 @@ router.post('/login', function(req, res, next) {
     });
 });
 
-router.put('/vote', function(req, res, next) {
+router.put('/vote', function (req, res, next) {
 
-    Post.insertVote(req.body, function(err, result) {
+    Post.insertVote(req.body, function (err, result) {
         if (err) {
             res.json(err);
         } else {
@@ -45,9 +49,9 @@ router.put('/vote', function(req, res, next) {
     });
 });
 
-router.delete('/vote', function(req, res, next) {
+router.delete('/vote', function (req, res, next) {
 
-    Post.deleteVote(req.body, function(err, result) {
+    Post.deleteVote(req.body, function (err, result) {
         if (err) {
             res.json(err);
         } else {
@@ -58,9 +62,9 @@ router.delete('/vote', function(req, res, next) {
     });
 });
 
-router.post('/vote', function(req, res, next) {
+router.post('/vote', function (req, res, next) {
 
-    Post.getVote(req.body, function(err, result) {
+    Post.getVote(req.body, function (err, result) {
 
         if (err) {
             res.json(err);
